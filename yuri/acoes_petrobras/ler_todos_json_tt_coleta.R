f6a4c0915f17ffa5cebb9d691a955f638af6d9eb
Yuri <yurijrdn@gmail.com>
2017-06-27 10:47:18 -0300

    Criação do método de coleta da movimentação da pert4, durante o mÊs de Dezembro.

--- yuri/acoes_petrobras/ler_todos_json_tt_coleta.R
+++ yuri/acoes_petrobras/ler_todos_json_tt_coleta.R
@@ -3,14 +3,14 @@ getPetr4_dataset <- function(){
 
       require(streamR)
       
-      lista_jsons <- dir(paste(path, '/acoes_petrobras/dadosPETR4', sep = ''))
+      lista_jsons <- dir(paste(path, '/acoes_petrobras/teste', sep = ''))
       
-      arquivos_json <- parseTweets(paste(path, "/acoes_petrobras/dadosPETR4/", 
-                                    lista_jsons[1], sep = ""))
+      arquivos_json <- parseTweets(paste(path, "/acoes_petrobras/teste/", 
+                                         lista_jsons[1], sep = ""))
       
       #Cria um data frame, com todos os arquivos json, do diretório escolhido em "lista_jsons"
       for(i in 2:length(lista_jsons)){
-            path_name <- paste(path, "/acoes_petrobras/dadosPETR4/",
+            path_name <- paste(path, "/acoes_petrobras/teste/",
                                lista_jsons[i], sep = "")
             tts <- parseTweets(path_name)
             arquivos_json <- merge(arquivos_json, tts, all = T)
--- yuri/acoes_petrobras/main.R
+++ yuri/acoes_petrobras/main.R
@@ -1,4 +1,4 @@
-path <- "C:\\Users\\Yuri\\Documents\\R"
+path <- "C:\\Users\\Yuri\\Documents\\Git\\StockMarket-DataAnalysis\\yuri"
 setwd(paste(path, "/acoes_petrobras", sep = ""))
 
 
@@ -9,9 +9,11 @@ source(file = paste(path, '/acoes_petrobras/timeSeries_getPetr4_dataset.R', sep 
 source(file = paste(path, '/acoes_petrobras/ler_todos_json_tt_coleta.R', sep = ''))
 source(file = paste(path, '/acoes_petrobras/Análise de texto/traducao_pct_sentimentos.R', sep = ''))
 source(file = paste(path, '/acoes_petrobras/analise_sentimental.R', sep = ''))
+source(file = paste(path, '/acoes_petrobras/acoes_yahoo_dezembro.R', sep = ''))
 
 
-data <- getPetr4_dataset()
+petr4 <- getPetr4_dataset()
+stoock <- get_yahoo_stock()
 dataset_token <- tokenize_text_data(data)
 lexical <- get_compare_lexical_table()
 sent <- sentimental_analisys(dataset_token, lexical)
